9271c52b8404f968a9524aee6a737428c12b49a6
Kai Engelhardt <kai@kaiengelhardt.com>
2024-04-26 22:10:10 +0200

    Clean up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "KEConstraints",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "KEConstraints",
-            targets: ["KEConstraints"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "KEConstraints"),
-        .testTarget(
-            name: "KEConstraintsTests",
-            dependencies: ["KEConstraints"]),
-    ]
+	name: "KEConstraints",
+	products: [
+		.library(
+			name: "KEConstraints",
+			targets: ["KEConstraints"]
+		),
+	],
+	targets: [
+		.target(
+			name: "KEConstraints"
+		),
+		.testTarget(
+			name: "KEConstraintsTests",
+			dependencies: ["KEConstraints"]
+		),
+	]
 )
